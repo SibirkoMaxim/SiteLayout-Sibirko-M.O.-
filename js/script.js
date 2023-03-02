4e6413f7ecd2ed6f2b5e7c332d81f801948e35df
Maksim Sibirko <hookieboom@yandex.ru>
2023-03-02 14:56:05 +0300

    Обновление Popup, Добавление новой страницы

--- js/script.js
+++ js/script.js
@@ -2,3 +2,8 @@ const loginbutton = document.querySelector('.login-button');
 loginbutton.addEventListener('click', () => {
     document.querySelector('.login-popup').classList.toggle('login-popup-active');
 });
+
+const crossbutton = document.querySelector('.cross-button');
+crossbutton.addEventListener('click', () => {
+    document.querySelector('.login-popup').classList.toggle('login-popup-active');
+});
